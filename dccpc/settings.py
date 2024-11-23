a4c5890e11e864c9b518aa9ddb83f4d499e4df5f
Monowar Hossain Khan <mhkha2022@gmail.com>
2024-11-24 01:48:16 +0600

    static and media root

--- dccpc/settings.py
+++ dccpc/settings.py
@@ -126,8 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = os.getenv('STATIC_ROOT')
 STATIC_URL = 'static/'
-MEDIA_ROOT = 'media/'
+MEDIA_ROOT = os.getenv('MEDIA_ROOT')
 MEDIA_URL = 'media/'
 
 # Default primary key field type
